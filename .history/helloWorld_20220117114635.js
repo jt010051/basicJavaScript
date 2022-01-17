27fc74b6e49684dcb7f48e3499c8142dd4612d7f
jt010051 <jonthomas.smith@hcl.com>
2022-01-17 11:48:19 -0500

    Basic JavaScript

---
+++ .history/helloWorld_20220117114635.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+
+var number = 5; //in-line comment
+
+/*
+Multi-Line Comment
+*/
---
+++ .history/helloWorld_20220117114658.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+var number = 5; //in-line comment
+
+/*
+Multi-Line Comment
+*/
+
+number=9;
